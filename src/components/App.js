73500fe9125b90adda5510bd82e1c56119ff971f
code0xff <ian.jungyong.um@gmail.com>
2019-07-10 13:21:21 +0900

    edit text of contents

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ export default class App extends Component {
       <div className='app'>
         <h1 className='title'>Electron React App</h1>
         <p className='contents'>
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save.
         </p>
       </div>
     )
