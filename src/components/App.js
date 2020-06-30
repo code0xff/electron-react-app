4be3a39f892aaff97daeed615a9d0ac712194498
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2020-06-30 23:50:55 +0900

    remove class title and contents'

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,8 @@ export default class App extends Component {
   render () {
     return (
       <div className='app'>
-        <h1 className='title'>Electron React App</h1>
-        <p className='contents'>
+        <h1>Electron React App</h1>
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
